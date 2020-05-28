dd525daedbdfb78fb28d6701c5228dec74eea71f
Lucas Leal <leallucasfonseca@gmail.com>
2020-05-28 14:03:25 -0300

    feat: Controller
    Desenvolvimento dos controllers

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,5 +1,8 @@
 from application import app
-from flask import render_template
+from flask import render_template, request
+from application.model.entity.video import Video
+from application.model.entity.categoria import Categoria
+from application.model.dao.categoria_dao import CategoriaDAO
 
 @app.route("/")
 @app.route("/home")
